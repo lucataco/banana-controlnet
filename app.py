0750d53401baec9e60c7240bfa4c02f8dd7660e4
Sethuraman <iramshiv@gmail.com>
2023-09-18 12:39:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,33 +6,37 @@ from PIL import Image
 from io import BytesIO
 from diffusers.utils import load_image
 from diffusers import UniPCMultistepScheduler, StableDiffusionControlNetPipeline, ControlNetModel
+from potassium import Potassium, Request, Response
 
+app = Potassium("my_app")
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
+@app.init
 def init():
-    global model
-    global controlnet
 
     controlnet = ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-canny", torch_dtype=torch.float16)
     model = StableDiffusionControlNetPipeline.from_pretrained(
         "runwayml/stable-diffusion-v1-5", controlnet=controlnet, safety_checker=None, torch_dtype=torch.float16
     )
 
+    context = {
+            "model": model,
+            "controlnet" : controlnet
+        }
+    
+        return context
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
-def inference(model_inputs:dict) -> dict:
-    global model
-    global controlnet
+@app.handler()
+def handler(context: dict, request: Request) -> Response:
 
     # Parse out your arguments
-    prompt = model_inputs.get('prompt', None)
-    negative_prompt = model_inputs.get('negative_prompt', None)
-    num_inference_steps = model_inputs.get('num_inference_steps', 20)
-    image_data = model_inputs.get('image_data', None)
-    if prompt == None:
-        return {'message': "No prompt provided"}
+    prompt = request.json.get("prompt")
+    negative_prompt = request.json.get("negative_prompt")
+    num_inference_steps = request.json.get("num_inference_steps")
+    image_data = request.json.get("image_data")
     
     # Run the model
     image = Image.open(BytesIO(base64.b64decode(image_data))).convert("RGB") 
@@ -48,23 +52,28 @@ def inference(model_inputs:dict) -> dict:
     canny_image.save(buffered,format="JPEG")
     canny_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
 
+    model = context.get("model")
+
     model.scheduler = UniPCMultistepScheduler.from_config(model.scheduler.config)
     model.enable_model_cpu_offload()
     model.enable_xformers_memory_efficient_attention()
-    output = model(
+    outputs = model(
         prompt,
         canny_image,
         negative_prompt=negative_prompt,
         num_inference_steps=num_inference_steps
     )
 
-    image = output.images[0]
+    image = outputs.images[0]
     buffered = BytesIO()
     image.save(buffered,format="JPEG")
     image_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
 
     # Return the results as a dictionary
-    return {
-        'canny_base64': canny_base64,
-        'image_base64': image_base64
-    }
+    return Response(
+        json = {"outputs": outputs}, 
+        status=200
+    )
+
+if __name__ == "__main__":
+    app.serve()
